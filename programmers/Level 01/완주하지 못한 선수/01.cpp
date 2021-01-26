8d14baf05046bcca75626ee3c462650fcbb03774
On <62434898+cks3066@users.noreply.github.com>
2021-01-26 16:48:35 +0900

    Create 01.cpp

---
+++ programmers/Level 01/완주하지 못한 선수/01.cpp
@@ -0,0 +1,17 @@
+#include <map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    map<string, int> m;
+
+    for (auto k : completion) {
+        m[k] += 1;
+    }
+
+    for (auto k : participant) {
+        m[k] -= 1;
+        if (m[k] < 0)
+            return k;
+    }
+}
