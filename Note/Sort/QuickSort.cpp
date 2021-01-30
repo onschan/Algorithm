abcfc47674b0700eb613b0fe9415894b928d2307
On <62434898+cks3066@users.noreply.github.com>
2021-01-30 18:31:41 +0900

    Create QuickSort.cpp

---
+++ Note/Sort/QuickSort.cpp
@@ -0,0 +1,31 @@
+# define SWAP(x, y, temp) ( (temp)=(x), (x)=(y), (y)=(temp) )
+
+void QuickSort(int* arr, int size, int start, int end) {
+    if (start >= end) {
+        return;
+    }
+    int pivot = start;
+    int i = start + 1;
+    int j = end;
+    int temp;
+
+    while (i <= j) {
+        while (arr[i] <= arr[pivot]) {
+            i++;
+        }
+        while (arr[j] >= arr[pivot] && j > start) {
+            j--;
+        }
+        if (i > j) {
+            SWAP(arr[j], arr[pivot], temp);
+        }
+        else {
+            SWAP(arr[j], arr[i], temp);
+        }
+
+        QuickSort(arr, size, start, j - 1);
+        QuickSort(arr, size, j + 1, end);
+    }
+
+    return;
+}
