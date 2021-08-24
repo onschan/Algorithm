7e1c44a007f434e080dc7c3201b1a89df7b2eb45
On <62434898+cks3066@users.noreply.github.com>
2021-08-24 23:50:00 +0900

    Create 딕셔너리로 숫자세기.py

---
+++ Note/Python/딕셔너리로 숫자세기.py
@@ -0,0 +1,20 @@
+input = "abacabaed"
+dic = {}
+
+
+def find_not_repeating_character(string):
+    # 이 부분을 채워보세요!
+    for i in string:
+        if (i in dic) is False:
+            dic[i] = 0
+        else:
+            dic[i] += 1
+
+    for i in dic:
+        if dic[i] == 0:
+            return i
+    return "중복없어"
+
+
+result = find_not_repeating_character(input)
+print(result)
